b5aa3e9c5cdcb3a38c842b179c7e3ff5d77d192d
Kentaro Kaneki <kentarokaneki@gmail.com>
2020-05-14 00:10:41 -0400

    Change logger library

--- src/LoggerWrapper.ts
+++ src/LoggerWrapper.ts
@@ -1,26 +1,28 @@
-import { getLogger, Logger, configure } from "log4js";
-configure({
-  appenders: {
-    everything: {
-      type: "file",
-      filename: "./all-the-logs.log",
-    },
-  },
-  categories: {
-    default: { appenders: ["everything"], level: "info" },
-  },
+import winston, { Logger, format } from "winston";
+const { combine, timestamp, json } = format;
+
+const logger = winston.createLogger({
+  level: "info",
+  format: combine(timestamp(), json()),
+  transports: [
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+    new winston.transports.Console(),
+  ],
 });
 
 export interface ILoggerWrapper {
   info(message: string): void;
   error(message: string): void;
-  fatal(message: string): void;
 }
 
 export default class LoggerWrapper implements ILoggerWrapper {
   private logger: Logger;
   constructor() {
-    this.logger = getLogger();
+    this.logger = logger;
   }
 
   info(message: string) {
@@ -29,9 +31,5 @@ export default class LoggerWrapper implements ILoggerWrapper {
 
   error(message: string) {
     this.logger.error(message);
-  }
-
-  fatal(message: string) {
-    this.logger.fatal(message);
   }
 }
--- test/mock/LoggerWrapperMock.ts
+++ test/mock/LoggerWrapperMock.ts
@@ -3,11 +3,10 @@ import { ILoggerWrapper } from "../../src/LoggerWrapper";
 export default class LoggerWrapperMock implements ILoggerWrapper {
   infoArgs: string[];
   errorArgs: string[];
-  fatalArgs: string[];
+
   constructor() {
     this.infoArgs = [];
     this.errorArgs = [];
-    this.fatalArgs = [];
   }
 
   info(message: string) {
@@ -16,9 +15,5 @@ export default class LoggerWrapperMock implements ILoggerWrapper {
 
   error(message: string) {
     this.errorArgs.push(message);
-  }
-
-  fatal(message: string) {
-    this.fatalArgs.push(message);
   }
 }
